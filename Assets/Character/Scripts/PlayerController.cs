6c52aa3a2d23795c0dec14e3bb68485a6211e369
Blake245 <jwillden@student.neumont.edu>
2025-02-04 20:11:54 -0700

    3d animation

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 3;
-    [SerializeField] float jumpForce = 3;
+    [SerializeField] PlayerData data;
     [SerializeField] Transform view;
+    [SerializeField] Animator animator;
 
     [SerializeField] LayerMask groundLayerMask;
 
     CharacterController controller;
     InputAction moveAction;
     InputAction jumpAction;
+    InputAction sprintAction;
     Vector2 movementInput = Vector2.zero;
     Vector3 velocity = Vector3.zero;
 
@@ -26,6 +27,10 @@ public class PlayerController : MonoBehaviour
         jumpAction.performed += OnJump;
         jumpAction.canceled += OnJump;
 
+        sprintAction = InputSystem.actions.FindAction("Sprint");
+        sprintAction.performed += OnSprint;
+        sprintAction.canceled += OnSprintRelease;
+
         controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -33,14 +38,35 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
         Vector3 movement = new Vector3(movementInput.x, 0, movementInput.y);
         movement = Quaternion.AngleAxis(view.rotation.eulerAngles.y, Vector3.up) * movement;
 
-        velocity.x = movement.x * speed;
-        velocity.z = movement.z * speed;
+        velocity.x = movement.x * data.speed;
+        velocity.z = movement.z * data.speed;
+
+        if (movement.sqrMagnitude > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), Time.deltaTime * data.turnRate);
+            //transform.forward = movement;
+        }
 
         velocity.y += Physics.gravity.y * Time.deltaTime;
-        controller.Move(velocity * speed *Time.deltaTime);
+        controller.Move(velocity * data.speed *Time.deltaTime);
+
+        // Set Animator
+        Vector3 v = velocity;
+        v.y = 0;
+        animator.SetFloat("Speed", v.magnitude);
+        animator.SetFloat("YVelocity", controller.velocity.y);
+        animator.SetBool("OnGround", controller.isGrounded);
+        Debug.Log($"Speed: {v.magnitude}");
+        //Debug.Log($"YVelocity: {controller.velocity.y}");
+        //Debug.Log($"Onground: {controller.isGrounded}");
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -52,7 +78,20 @@ public class PlayerController : MonoBehaviour
     {
         if (ctx.phase == InputActionPhase.Performed && controller.isGrounded)
         {
-            velocity.y = jumpForce;
+            //velocity.y = jumpForce;
+            velocity.y = Mathf.Sqrt(-2 * data.gravity * data.jumpHeight);
         }
+    }
+
+    public void OnSprint(InputAction.CallbackContext ctx)
+    {
+        if (controller.isGrounded && data.speed == 1.33f)
+        {
+            data.speed = 3;
+        }
+    }
+    public void OnSprintRelease(InputAction.CallbackContext ctx)
+    {
+        data.speed = 1.33f;
     }
 }
